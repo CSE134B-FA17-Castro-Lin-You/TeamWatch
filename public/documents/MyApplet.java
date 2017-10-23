e61a3c70d7b9a9ed7c8fd62fa2243f71cb15fb06
vaiolis <harrisonllin@gmail.com>
2017-10-22 17:48:48 -0700

    Other tags

---
+++ public/documents/MyApplet.java
@@ -0,0 +1,7 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class MyApplet extends Applet{  
+  public void paint(Graphics g){
+    g.drawString("Hi, I'm a Java applet!",120,100);
+  }
+}
